086ae56a32eb80744f2805710ca515da07a269e1
nemundo <urs.lang@gmail.com>
2019-05-09 14:29:30 +0200

    .

--- test/IgcReader.php
+++ test/IgcReader.php
@@ -1,15 +1,21 @@
 <?php
 
-require '../vendor/autoload.php';
+require __DIR__.'/../../../config.php';
 
-$filename = '190331_NKN_01.igc';
+$performance = new \Nemundo\App\Performance\PerformanceStopwatch();
 
+
+$performance->startStopwatch('igc');
+
+$filename = __DIR__.'/190331_NKN_01.igc';
 $reader = new \Nemundo\Igc\IgcReader($filename);
-
-(new \Nemundo\Core\Debug\Debug())->write('Date: '.$reader->date->getIsoDateFormat());
-
+//(new \Nemundo\Core\Debug\Debug())->write('Date: '.$reader->date->getIsoDateFormat());
 foreach ($reader->getData() as $igcCoordinate) {
-    (new \Nemundo\Core\Debug\Debug())->write($igcCoordinate);
+    //(new \Nemundo\Core\Debug\Debug())->write($igcCoordinate);
 }
+
+
+
+$performance->writeStopwatch();
 
 
